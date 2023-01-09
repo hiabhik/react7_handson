c776aafcefd618063c8b08aca2bfe0637994d793
hiabhik <109613142+hiabhik@users.noreply.github.com>
2023-01-09 17:13:59 +0530

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,46 @@
+const Student=[
+    {
+        "id": 0,
+        "Name": "Bibek",
+        "Age": "25",
+        "Course": "Python",
+        "Batch": "Jan"
+    },
+    {
+        "id": 1,
+        "Name": "Sarah",
+        "Age": "22",
+        "Course": "Java",
+        "Batch": "Feb"
+    },
+    {
+        "id": 2,
+        "Name": "Jonny",
+        "Age": "25",
+        "Course": "c#",
+        "Batch": "August"
+    },
+    {
+        "id": 3,
+        "Name": "Sonam",
+        "Age": "24",
+        "Course": "C",
+        "Batch": "March"
+    },
+    {
+        "id": 4,
+        "Name": "Aditya",
+        "Age": 19,
+        "Course": "Full Stack",
+        "Batch": "July"
+    },
+    {
+        "id": 5,
+        "Name": "Mohan",
+        "Age": 24,
+        "Course": "Frontend",
+        "Batch": "September"
+    },
+]
+
+export default Student;
